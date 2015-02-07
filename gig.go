13574ac3e3859ce2e775a076bad7fee83663a496
Chuck Forbes <cforbes@combarriers.com>
2015-02-07 08:18:17 -0800

    initial commit

---
+++ gig.go
@@ -0,0 +1,12 @@
+package main
+
+import  (
+           "fmt"
+           "github.com/perlwonk/stringutil"
+           // next, let's create some more library functions and add them here.
+)
+
+
+func main() {
+    fmt.Printf(stringutil.Reverse("!oG, olleH"))
+}
