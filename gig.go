8467f8a465e5c16fd1e4aadf7a1824857d652436
Chuck Forbes <cforbes@combarriers.com>
2015-02-07 10:43:45 -0800

    Added a comment

--- gig.go
+++ gig.go
@@ -4,6 +4,7 @@ import  (
            "fmt"
            "github.com/perlwonk/stringutil"
            // next, let's create some more library functions and add them here.
+           // this is another comment
 )
 
 
